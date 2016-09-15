5bb7e00b273ed96232d788d356887b7e5a5f5742
Carlos Chitty <chitty.carlosj@gmail.com>
2016-09-15 14:08:01 -0400

    Bonus: Not counting empty items, coloring message and texbox according to the message content.

--- module1/app.js
+++ module1/app.js
@@ -4,17 +4,35 @@
 angular.module('LunchCheck', [])
 
 .controller('LunchCheckController', function($scope) {
-    $scope.message = ""
+    $scope.message = "";
+    $scope.message_class = "";
+    $scope.border_color = "";
 
     $scope.displayMessage = function() {
-        if ($scope.foods === undefined || $scope.foods.length === 0) {
-            $scope.message = "Please enter data first"
+        if ($scope.foods === undefined) {
+            $scope.message = "Please enter data first";
+            $scope.message_class = "alert alert-danger";
+            $scope.border_color = "red";
         } else {
             var foods = $scope.foods.split(",");
-            if (foods.length > 3) {
-                $scope.message = "Too much!"
+            var number_of_items = foods.length;
+            for (var i=0; i<foods.length; i++) {
+                if (foods[i].trim() === "")
+                    number_of_items--;
+            }
+            if (number_of_items === 0) {
+                $scope.message = "Please enter data first";
+                $scope.message_class = "alert alert-danger";
+                $scope.border_color = "red";
+            }
+            else if (number_of_items > 3) {
+                $scope.message = "Too much!";
+                $scope.message_class = "alert alert-warning";
+                $scope.border_color = "orange";
             } else {
                 $scope.message = "Enjoy!"
+                $scope.message_class = "alert alert-success"
+                $scope.border_color = "green";
             }
         }
     };
