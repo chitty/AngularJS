9871d0641d4bc1e1843cd2e3216a6d46bf8aba53
Carlos Chitty <chitty.carlosj@gmail.com>
2016-10-19 12:46:45 -0400

    Declared Controller, Service and Directive to be used

---
+++ module3/app.js
@@ -0,0 +1,54 @@
+(function() {
+'use strict';
+
+angular.module("NarrowItDownApp", [])
+.controller('NarrowItDownController', NarrowItDownController)
+.service('MenuSearchService', MenuSearchService)
+.directive('foundItems', FoundItemsDirective)
+.constant('ApiBasePath', "http://davids-restaurant.herokuapp.com");
+
+function FoundItemsDirective() {
+  var ddo = {
+    restrict: 'E',
+    templateUrl: 'foundItems.html'
+  };
+
+  return ddo;
+}
+
+
+NarrowItDownController.$inject = ['$scope', 'MenuSearchService'];
+function NarrowItDownController($scope, MenuSearchService) {
+  var menu = this;
+
+  var promise = MenuSearchService.getMatchedMenuItems($scope.searchTerm);
+
+  promise.then(function (response) {
+    menu.found = response.data.menu_items;
+    console.log(menu.found);
+  })
+  .catch(function (error) {
+    console.log("Something went terribly wrong.");
+  });
+}
+
+
+MenuSearchService.$inject = ['$filter', '$http', 'ApiBasePath']
+function MenuSearchService($filter, $http, ApiBasePath) {
+  var service = this;
+
+  service.getMatchedMenuItems = function(searchTerm) {
+    var response = $http({
+      method: "GET",
+      url: (ApiBasePath + "/menu_items.json")
+    })
+
+    //var full_menu = response.data.menu_items;
+    //var filtered_menu = $filter('filter')(full_menu, {description: searchTerm});
+
+    return response;
+
+  }
+}
+
+})();
