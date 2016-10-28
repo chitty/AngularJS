a8d598dc11808095a79e4bf2032a8aab0fedc126
Carlos Chitty <chitty.carlosj@gmail.com>
2016-10-28 16:06:29 -0400

    [M3] Solution using components instead of directives

---
+++ module3-loader/app.js
@@ -0,0 +1,75 @@
+(function() {
+'use strict';
+
+angular.module("NarrowItDownApp", [])
+.controller('NarrowItDownController', NarrowItDownController)
+.service('MenuSearchService', MenuSearchService)
+.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com")
+.component('foundItems', {
+  templateUrl: 'foundItems.html',
+  bindings:{
+    found: '<foundItems',
+    title: '@title',
+    onRemove: '&'    
+  }
+});
+
+
+NarrowItDownController.$inject = ['$scope', 'MenuSearchService'];
+function NarrowItDownController($scope, MenuSearchService) {
+  var menu = this;
+
+  menu.getMatchedMenuItems = function (searchTerm) {
+    if (searchTerm === undefined || searchTerm.length == 0) {
+      menu.found = {};
+      updateFoundItems();
+    } else {
+      var promise = MenuSearchService.getMatchedMenuItems(searchTerm);
+
+      promise.then(function (response) {
+        menu.found = response;
+        updateFoundItems();
+      })
+      .catch(function (error) {
+        console.log("Something went terribly wrong.");
+      });
+    }
+  }
+
+  menu.removeItem = function(index) {
+    menu.found.splice(index, 1);
+    updateFoundItems();
+  }
+
+  function updateFoundItems() {
+    if (menu.found.length === undefined || menu.found.length === 0) {
+      menu.title = "Nothing found"
+    } else if (menu.found.length === 1) {
+      menu.title = menu.found.length + " item found";
+    } else {
+      menu.title = menu.found.length + " items found";
+    }
+  }
+
+}
+
+
+MenuSearchService.$inject = ['$filter', '$http', 'ApiBasePath']
+function MenuSearchService($filter, $http, ApiBasePath) {
+  var service = this;
+
+  service.getMatchedMenuItems = function(searchTerm) {
+    var httpPromise = $http({
+      method: "GET",
+      url: (ApiBasePath + "/menu_items.json")
+    })
+
+    return httpPromise.then( function(response) {
+      var full_menu = response.data.menu_items;
+      var filtered_menu = $filter('filter')(full_menu, {description: searchTerm});
+      return filtered_menu;
+    })
+  }
+}
+
+})();
---
+++ module3-loader/readme.md
@@ -0,0 +1,9 @@
+# AngularJS third project _loader_
+
+Angular API calls to narrow down search over a menu. Following [this instructions](https://github.com/jhu-ep-coursera/fullstack-course5/blob/master/assignments/assignment3/Assignment-3.md).
+
+_Developed using Angular.js version: 1.5.8_
+
+## New concepts used
+
+- Components / Modules
